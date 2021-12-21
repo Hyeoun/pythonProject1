7df1ef9291d48751e9e3d0ce9f8a0a8d5f097157
Hyeoun <ing02084@naver.com>
2021-12-21 12:28:23 +0900

    change netflix_contents_insert.py

--- exam32_SQL/netflix_contents_insert.py
+++ exam32_SQL/netflix_contents_insert.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import pymysql
 import numpy as np
+import pickle
 
-def insert(sql):
-    conn = pymysql.connect(
+df = pd.read_csv('../datasets/netflix_titles.csv')
+print(df.head())
+df.info()
+
+df.fillna('', inplace=True)
+df = df.replace("'", "%\\'")
+df = df.replace('"', '%\\"')
+
+conn = pymysql.connect(
         user='root',
         passwd='1q2w3e4r',
         host='127.0.0.1',
@@ -11,42 +19,44 @@ def insert(sql):
         db='netflix',
         charset='utf8'
     )
+
+errors = []
+for i in range(len(df)):
     try:
+        a = []
+        for j in range(12):
+            if j != 7:
+                temp = df.iloc[i, j].replace("'", "\\'")
+                temp = df.iloc[i, j].replace('"', '\\"')
+                a.append('null' if temp == '' else '"{}"'.format(temp))
+            else:
+                a.append(df.iloc[i, j])
+        sql = 'insert into netflix_contents value({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'.format(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
+        # sql = 'insert into netflix_contents value({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'.format(
+        #     '"{}"'.format(df.iloc[i, 0]),
+        #     '"{}"'.format(df.iloc[i, 1]),
+        #     '"{}"'.format(df.iloc[i, 2]),
+        #     'null' if df.iloc[i, 3] == '' else '"{}"'.format(df.iloc[i, 3]),
+        #     'null' if df.iloc[i, 4] == '' else '"{}"'.format(df.iloc[i, 4]),
+        #     'null' if df.iloc[i, 5] == '' else '"{}"'.format(df.iloc[i, 5]),
+        #     'null' if df.iloc[i, 6] == '' else '"{}"'.format(df.iloc[i, 6]),
+        #     df.iloc[i, 7],
+        #     'null' if df.iloc[i, 8] == '' else '"{}"'.format(df.iloc[i, 8]),
+        #     'null' if df.iloc[i, 9] == '' else '"{}"'.format(df.iloc[i, 9]),
+        #     '"{}"'.format(df.iloc[i, 10]),
+        #     '"{}"'.format(df.iloc[i, 11]))
+        # print(sql)
         with conn.cursor() as cursor:
             cursor.execute(sql)
         conn.commit()
     except:
-        print('conn error')
-    finally:
-        conn.close()
+        print(sql)
+        errors.append(i)
+        print(i)
 
-df = pd.read_csv('../datasets/netflix_titles.csv')
-print(df.head())
-df.info()
-df.fillna('', inplace=True)
-
-for i in range(len(df)):
-    a = []
-    for j in range(12):
-        if j != 7:
-            a.append('null' if df.iloc[i, j] == '' else '"{}"'.format(df.iloc[i, j]))
-        else:
-            a.append(df.iloc[i, j])
-    sql = 'insert into netflix_contents value({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'.format(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
-    # sql = 'insert into netflix_contents value({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'.format(
-    #     '"{}"'.format(df.iloc[i, 0]),
-    #     '"{}"'.format(df.iloc[i, 1]),
-    #     '"{}"'.format(df.iloc[i, 2]),
-    #     'null' if df.iloc[i, 3] == '' else '"{}"'.format(df.iloc[i, 3]),
-    #     'null' if df.iloc[i, 4] == '' else '"{}"'.format(df.iloc[i, 4]),
-    #     'null' if df.iloc[i, 5] == '' else '"{}"'.format(df.iloc[i, 5]),
-    #     'null' if df.iloc[i, 6] == '' else '"{}"'.format(df.iloc[i, 6]),
-    #     df.iloc[i, 7],
-    #     'null' if df.iloc[i, 8] == '' else '"{}"'.format(df.iloc[i, 8]),
-    #     'null' if df.iloc[i, 9] == '' else '"{}"'.format(df.iloc[i, 9]),
-    #     '"{}"'.format(df.iloc[i, 10]),
-    #     '"{}"'.format(df.iloc[i, 11]))
-    print(sql)
+conn.close()
+with open('./errors_description.pickle', 'wb') as f:
+    pickle.dump(errors, f)
 
 # print(df.listed_in.head())
 # print(df.type.unique())
